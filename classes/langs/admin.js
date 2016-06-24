2e70706091a20529298352fb8dc0e55b7ac60cf6
Piotr Podstawski <piotr@webkameleon.com>
2016-06-24 21:43:22 +0200

    iframe

--- classes/langs/admin.js
+++ classes/langs/admin.js
@@ -25,16 +25,14 @@ var Web = function(com,ini,logger,callback) {
     return {
         //Web router
         'request': function(request,response) {
-
             var path = url.parse(request.url).pathname;
-            
-            
+ 
             switch(path){
                 case '/check-web':
                     response.setHeader('Access-Control-Allow-Origin', '*');
-                    response.write('OK,'+request.headers.host, "utf8");
+                    response.write('OK,'+request.headers.host);
                     response.end();            
-                
+                    break;
                 default:
                     //response.writeHead(404);
                     response.write("opps this doesn't exist - 404");
