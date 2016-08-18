3ef4d41c34d0ccafbbecce084c1fb0613db1d20e
Piotr <piotr@webkameleon.com>
2016-08-18 09:43:41 +0200

    dane

--- promienko.sh
+++ promienko.sh
@@ -2,7 +2,7 @@
 
 while [ "1" = "1" ]
 do
-	node --max_old_space_size=20 app >>/sd/promienko.log 2>>/sd/promienko.err 
+	node --max_old_space_size=30 app >>/sd/promienko.log 2>>/sd/promienko.err 
 	echo "Restart `date`" >>/sd/promienko.err
 	killall -TERM ssh
 	sleep 1
