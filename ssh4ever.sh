1965db04e48600f7fe39c316697e21aa068b73d6
Piotr <piotr@webkameleon.com>
2016-08-27 22:42:53 +0200

    rc.local

--- ssh4ever.sh
+++ ssh4ever.sh
@@ -3,7 +3,7 @@
 while [ "1" = "1" ]
 do
 
-	ssh -NT -o TCPKeepAlive=yes -o ServerAliveInterval=60 -R 4022:localhost:22 home@home.webkameleon.com /home/home/home.sh 4022
+	ssh -nT -o TCPKeepAlive=yes -o ServerAliveInterval=60 -R 4022:localhost:22 home@home.webkameleon.com /home/home/home.sh 4022
 	echo failed
 	sleep 5
 done
