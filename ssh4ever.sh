d13c4e3030132eb2178bf391b6f88ad68a68ff15
piotr <piotr@webkameleon.com>
2016-08-30 09:10:42 +0000

    ssh

--- ssh4ever.sh
+++ ssh4ever.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+if [ ! -f `dirname $0`/conf/ssh.port ]
+then
+	exit
+fi
+
+dir=`dirname $0`
+port=`cat $dir/conf/ssh.port | awk '{print $1}'`
+
+
 while [ "1" = "1" ]
 do
 
-	ssh -nT -o TCPKeepAlive=yes -o ServerAliveInterval=60 -R 4022:localhost:22 home@home.webkameleon.com /home/home/home.sh 4022
+	ssh -nT -o TCPKeepAlive=yes -o ServerAliveInterval=60 -R $port:localhost:22 home@home.webkameleon.com /home/home/home.sh $port
 	sleep 5
 done
