3a91a001760d8b9bd8872c951de1f0723bfecd04
Piotr Podstawski <piotr@webkameleon.com>
2016-09-01 17:38:41 +0000

    ok

---
+++ classes/common/hsettings.js
@@ -0,0 +1,50 @@
+var fs = require('fs');
+var exec = require('child_process').exec;
+
+var _settings=null;
+
+module.exports = function(s) {
+    var dir=__dirname+'/../../conf';
+    var file=dir+'/web.json';
+    var wifi=dir+'/wifi';
+    
+    if (s) {
+        if (!_settings) _settings=s;
+        else for (var k in s) _settings[k]=s[k];
+        
+        fs.writeFileSync(file,JSON.stringify(_settings));
+        try {
+            var e=exec('fsync '+file);
+        } catch(e) {
+            
+        }
+        
+        if (s.ssid ) {
+            fs.writeFileSync(wifi,_settings.ssid+' '+_settings.wifipass);
+            
+            try {
+                var e=exec('fsync '+wifi);
+                exec('reboot');
+            } catch(e) {
+            
+            }
+        } else if (!_settings.ssid) {
+            try {
+                fs.unlinkSync(wifi);
+            } catch(e) {
+                
+            }
+        }
+    }
+    
+    if (!_settings) {
+        try{
+            var d = fs.readFileSync(file);
+            _settings = JSON.parse(d);
+        } catch(e) {
+            _settings={};
+        }
+    }
+    
+    return _settings;
+}
