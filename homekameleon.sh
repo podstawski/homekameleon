b7fc843a8097aa72f064e2452f7674888663823e
Piotr <piotr@webkameleon.com>
2016-10-25 18:06:40 +0200

    ok

--- homekameleon.sh
+++ homekameleon.sh
@@ -5,8 +5,8 @@ cd `dirname $0`
 while [ "1" = "1" ]
 do
 	# --max_old_space_size=30
-	node --expose-gc --max_old_space_size=45 app >>/sd/promienko.log 2>>/sd/promienko.err 
-	echo "Restart `date`" >>/sd/promienko.err
+	node --expose-gc --max_old_space_size=45 app >>/sd/homekameleon.log 2>>/sd/homekameleon.err 
+	echo "Restart `date`" >>/sd/homekameleon.err
 	killall -TERM ssh
 	sleep 1
 done
