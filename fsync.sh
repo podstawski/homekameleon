624a0371b189c2ae51a485384ab4c1bd96f859d4
Piotr <piotr@webkameleon.com>
2017-01-26 20:51:13 +0100

    my

--- fsync.sh
+++ fsync.sh
@@ -2,5 +2,5 @@ fsync /sd/conf/*
 fsync /sd/logs/*
 cp /sd/conf/*.db /homekameleon/conf
 cp /sd/conf/ios.json /homekameleon/conf
-fscync /homekameleon/conf
+fsync /homekameleon/conf
 touch /tmp/homekameleon.fsync
