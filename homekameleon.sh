b4670436b65a06b62633c1a727e2355aa7254a1d
Piotr <piotr@webkameleon.com>
2017-02-06 14:35:32 +0100

    ok

--- homekameleon.sh
+++ homekameleon.sh
@@ -11,7 +11,7 @@ fi
 while [ "1" = "1" ]
 do
 	# --max_old_space_size=30
-	node --expose-gc --max_old_space_size=45 app >>/tmp/homekameleon.log 2>>/tmp/homekameleon.err 
+	node --expose-gc --max_old_space_size=45 app >/tmp/homekameleon.log 2>>/tmp/homekameleon.err 
 	echo "Restart `date`" >>/tmp/homekameleon.err
 	killall -TERM ssh
 	sleep 1
