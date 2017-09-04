5ab68e34af11283745f1fa931954b153f2ca16f8
Piotr <piotr@webkameleon.com>
2017-09-04 17:12:30 +0200

    ok

--- piwnica.sh
+++ piwnica.sh
@@ -1,1 +1,11 @@
-etherwake 00:24:8c:a5:46:fc
+for i in $(seq 2 50)
+do
+	ping -c 1 -w 1 192.168.0.$i >/dev/null &
+done
+
+wait
+
+ip=`cat /proc/net/arp | grep $1 | awk '{print $1}'`
+etherwake $1
+
+ssh -o ConnectTimeout=3 pudel@$ip sudo halt -p
