62d0b0b6559c67d05ac9a89135a287130adeb286
Piotr <piotr@webkameleon.com>
2018-02-23 16:51:03 +0100

    interer-indexOf

--- fsync.sh
+++ fsync.sh
@@ -4,3 +4,15 @@ cp /sd/conf/*.db /homekameleon/conf
 cp /sd/conf/ios.json /homekameleon/conf
 fsync /homekameleon/conf
 touch /tmp/homekameleon.fsync
+
+if [ "$1" = "" ]
+then
+	#exit
+	echo
+fi
+
+sleep 5
+killall -TERM node
+sleep 5
+reboot
+
--- homekameleon.sh
+++ homekameleon.sh
@@ -10,22 +10,32 @@ fi
 
 # --max_old_space_size=30
 node --expose-gc --max_old_space_size=45 app >/tmp/homekameleon.log 2>>/tmp/homekameleon.err &
-sleep 10
+sleep 60
+counter=0
 
 while [ "1" = "1" ]
 do
+	echo "$counter" > /tmp/homekameleon.hb.test
 	touch /tmp/homekameleon.hb.test
-	sleep 10
+	sleep 20
 	if [ /tmp/homekameleon.hb.test -nt /tmp/homekameleon.hb ]
 	then
+		counter=`expr $counter + 1`
 		pid=`cat /tmp/homekameleon.pid`
-		kill -TERM $pid
+		#kill -TERM $pid
+		killall -TERM node
 		sleep 2
-		kill -9 $pid
+		#kill -9 $pid
+		killall -9 node
 		sleep 1
 		echo "Restart `date`" >>/tmp/homekameleon.err
+		if [ "$counter" = "5" ]
+		then
+			/homekameleon/fsync.sh 1
+			exit
+		fi
 		node --expose-gc --max_old_space_size=45 app >/tmp/homekameleon.log 2>>/tmp/homekameleon.err &
-		sleep 10
+		sleep 60
 	fi
 
 done
